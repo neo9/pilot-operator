fcca30d1c3f8d174ac7a56f851b56e17833071bb
Victor Boissiere <victor.boissiere@neo9.fr>
2019-06-26 11:16:10 +0200

    refactor: Extract tests main logic

---
+++ test/e2e/helpers/helpers.go
@@ -0,0 +1,51 @@
+package helpers
+
+import (
+	"github.com/neo9/pilot-operator/test/e2e/applicationv1alpha1"
+	"github.com/operator-framework/operator-sdk/pkg/test"
+
+	"github.com/neo9/pilot-operator/pkg/apis"
+	"testing"
+	"time"
+	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+)
+
+var (
+	RetryInterval        = time.Second * 5
+	Timeout              = time.Second * 60
+	CleanupRetryInterval = time.Second * 1
+	CleanupTimeout       = time.Second * 5
+	operatorName         = "pilot-operator"
+)
+
+func GetClusterContext(t *testing.T) (string, *test.TestCtx) {
+	sampleList := applicationv1alpha1.GetSampleList()
+	crdError := test.AddToFrameworkScheme(apis.AddToScheme, &sampleList)
+	if crdError != nil {
+		t.Fatalf("failed to add custom resource scheme to framework: %v", crdError)
+	}
+	ctx := test.NewTestCtx(t)
+	defer ctx.Cleanup()
+	crError := ctx.InitializeClusterResources(&test.CleanupOptions{
+		TestContext: ctx,
+		Timeout: CleanupTimeout,
+		RetryInterval: CleanupRetryInterval,
+	})
+	if crError != nil {
+		t.Fatalf("failed to initialize cluster resources: %v", crError)
+	}
+
+	t.Log("Initialized cluster resources")
+	namespace, err := ctx.GetNamespace()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	f := test.Global
+	err = e2eutil.WaitForOperatorDeployment(t, f.KubeClient, namespace, operatorName, 1, RetryInterval, Timeout)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return namespace, ctx
+}
---
+++ test/e2e/web_test.go
@@ -0,0 +1,41 @@
+package e2e
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/operator-framework/operator-sdk/pkg/test"
+	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+
+	"github.com/neo9/pilot-operator/test/e2e/applicationv1alpha1"
+	"github.com/neo9/pilot-operator/test/e2e/helpers"
+)
+
+func TestSimpleWeb(t *testing.T) {
+	namespace, ctx := helpers.GetClusterContext(t)
+	// get global framework variables
+	f := test.Global
+	application := applicationv1alpha1.GetSampleWebApplication(namespace)
+	err := f.Client.Create(context.TODO(), &application, &test.CleanupOptions{
+		TestContext: ctx,
+		Timeout: helpers.Timeout,
+		RetryInterval: helpers.RetryInterval,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = e2eutil.WaitForDeployment(t, f.KubeClient, namespace, application.Name, 1, time.Second*5, time.Second*30)
+
+	application.Spec.Replicas = 4
+	err = f.Client.Update(context.TODO(), &application)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = e2eutil.WaitForDeployment(t, f.KubeClient, namespace, application.Name, 4, time.Second*5, time.Second*30)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
