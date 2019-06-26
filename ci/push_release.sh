a7fbb8cd78ab59416c099a3b006842145fba9e36
Victor Boissiere <victor.boissiere@neo9.fr>
2019-06-26 16:49:23 +0200

    travis: Fix release docker authentication

--- ci/push_release.sh
+++ ci/push_release.sh
@@ -2,6 +2,7 @@
 
 if ! [ -z "$TRAVIS_TAG" ]; then
     echo "Release $TRAVIS_TAG"
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_LOGIN" --password-stdin
     docker tag neo9sas/pilot-operator:latest neo9sas/pilot-operator:$TRAVIS_TAG
     docker push neo9sas/pilot-operator:$TRAVIS_TAG
 fi
