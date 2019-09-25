63c3012ec78ec32a80ed3b82e60bd514fac9e511
Victor Boissiere <victor.boissiere@neo9.fr>
2019-09-25 17:22:49 +0200

    ci: Remove scheduler tests

--- ci/tests.sh
+++ ci/tests.sh
@@ -3,7 +3,6 @@
 namespace="test-$(git rev-parse --short HEAD)-$TRAVIS_BUILD_ID"
 
 kubectl create namespace $namespace
-operator-sdk test local ./test/e2e/schedulerv1alpha1 --up-local --namespace $namespace
 operator-sdk test local ./test/e2e/applicationv1alpha1 --up-local --namespace $namespace
 exit_code=$?
 
